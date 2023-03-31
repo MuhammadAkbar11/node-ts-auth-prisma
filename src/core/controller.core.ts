6365e41bb17ce72ce14769924dad3075bb64ae85
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-03-31 19:23:43 +0700

    feat: custom error methods

--- src/core/controller.core.ts
+++ src/core/controller.core.ts
@@ -1,6 +1,9 @@
 // import { PrismaClient } from "@prisma/client";
+import { NextFunction } from "express";
 
 import { HTTP_STATUS_CODE } from "../configs/vars.config";
+import BaseError from "../helpers/error.helper";
+import { ErrorData } from "../utils/types/interfaces";
 
 export class CoreController {
   // public prisma: PrismaClient;
@@ -8,5 +11,18 @@ export class CoreController {
 
   constructor() {
     // this.prisma = new PrismaClient();
+  }
+
+  protected error(
+    name: string | null,
+    statusCode: number,
+    message: string,
+    errors: ErrorData = {}
+  ) {
+    throw new BaseError(name, statusCode, message, errors);
+  }
+
+  protected nextError(next: NextFunction, error: any) {
+    next(BaseError.transformError(error));
   }
 }
