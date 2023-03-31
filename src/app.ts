8078eb3487fe4d23bd0cfbb7387df163268a7699
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-03-31 19:23:18 +0700

    refactor: setup pino-http logger and error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express from "express";
 import DemoRouter from "./app/demo/demo.routes";
+import pinoHttpLogger from "./middlewares/logging.middleware";
+import { ENV_STATIC_FOLDER_PATH, STATIC_FOLDER } from "./configs/vars.config";
+import * as ENV from "./configs/vars.config";
+import {
+  logErrorMiddleware,
+  returnError404Middleware,
+  returnErrorMiddleware,
+} from "./middlewares/error.middleware";
+import { AutoBind } from "./utils/utils";
 
 class App {
   public server;
@@ -8,15 +17,28 @@ class App {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.errorMiddlewares();
   }
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(pinoHttpLogger);
+    this.server.use(express.static(STATIC_FOLDER));
+    if (ENV.MODE !== "production") {
+      this.server.use(express.static(ENV_STATIC_FOLDER_PATH));
+    }
   }
 
+  @AutoBind
   routes() {
     const demoRouter = new DemoRouter();
     this.server.use("/", demoRouter.router);
+  }
+
+  errorMiddlewares() {
+    this.server.use(logErrorMiddleware);
+    this.server.use(returnError404Middleware);
+    this.server.use(returnErrorMiddleware);
   }
 }
 
