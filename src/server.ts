461ba973ea88f23bc96e5c7999852f0aebf1805f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-04-04 22:47:27 +0700

    feat: corn job for delete expired session in database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,29 @@
 import { PORT, SERVER_URL, dotenvConfig } from "./configs/vars.config";
+import cron from "node-cron";
 import app from "./app";
 import logger from "./configs/logger.config";
+import { dateUTC } from "./configs/date.config";
+import prisma from "./configs/prisma.config";
 
 dotenvConfig;
 
-app.listen(PORT, () => logger.info(`[SERVER] app running ${SERVER_URL}`));
+app.listen(PORT, () => logger.info(`[SERVER] app running on ${SERVER_URL}`));
+
+cron.schedule(
+  "* * * * *",
+  async () => {
+    logger.info("[SERVER][SCHEDULE] Checking expired sessions...");
+    const expiredSessions = await prisma.session.deleteMany({
+      where: {
+        expired: {
+          lt: dateUTC().toISOString(),
+        },
+      },
+    });
+    logger.info(expiredSessions, "[SERVER][SCHEDULE] Deleted expired sessions");
+  },
+  {
+    timezone: "Asia/Jakarta",
+    scheduled: true,
+  }
+);
